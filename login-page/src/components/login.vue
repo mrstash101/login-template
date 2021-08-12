0aeca7491edbe737a2d7092bd42be0f9551f6068
mrstash101 <leotanaka83@gmail.com>
2021-08-12 20:47:55 +0900

    upd

---
+++ login-page/src/components/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <section class="hero is-light is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centerd">
+          <articile v-show="errorMessage" class="message is-warning">
+            <div class="message-body">
+              {{$t(errorMessage) }}
+            </div>
+          </articile>
+          <div class="column is-4 is-offset-4">
+            <div class="box">
+              <div class="field">
+                <div class="control"
+</template>
