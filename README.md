8ad8634974ff23743574eea864cadbb67f9b3c61
mrstash101 <leotanaka83@gmail.com>
2021-08-30 15:48:24 +0900

    upd

--- README.md
+++ README.md
@@ -8,3 +8,26 @@
 [RailsのAPI開発](https://qiita.com/kumagaias/items/250e7d6cf46966e4c8c2)
 
 [デザイン重視の高いログインページ](https://inokawablog.org/code/vuetify-auth-template/)
+
+src/
+├── App.vue
+├── assets
+│   └── logo.png
+├── components
+│   ├── error.vue
+│   ├── login.vue
+│   ├── logout.vue
+│   ├── menu.vue
+│   └── top.vue
+├── lang
+│   ├── index.js
+│   └── messages.json # エラーメッセージ等、文言をここに
+├── main.js
+├── router
+│   └── index.js # ルーティング定義
+└── store
+    ├── index.js
+    └── modules
+        ├── auth.js # token を管理
+        ├── http.js # API と通信する際はコレを使う
+        └── message.js # エラーメッセージの管理
--- login-page/src/views/Home.vue
+++ login-page/src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
-  <hello-world />
+  <h3>Welcome {{ $store.state.userId }}</h3>
+  <p>{{message}}</p>
 </template>
-
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+export default {
+  data() {
+    return {
+      message: ""
+    };
+  },
+  created: function() {
+    this.fetchHello();
+  },
+  methods: {
+    fetchHello() {
+      const uri = "http://localhost:8080/hello";
+      this.axios.get(uri).then(response => {
+        this.message = response.data.message;
+      });
+    }
   }
+};
 </script>
