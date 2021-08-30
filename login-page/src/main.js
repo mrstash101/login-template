2bb62fbc182cb8d17adbc9a58a2a513eba7798b2
mrstash101 <leotanaka83@gmail.com>
2021-08-30 17:29:10 +0900

    upd

--- login-page/src/main.js
+++ login-page/src/main.js
@@ -4,12 +4,7 @@ import Vue from 'vue'
 import VeeValidate from 'vee-validate'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD
 import vuetify from './plugins/vuetify'
-=======
-import store from './store'
-import lang from '/lang'
->>>>>>> 821da11c9f2b5c595df93d7a707310cf31973097
 
 Vue.use(VeeValidate)
 Vue.config.productionTip = false
